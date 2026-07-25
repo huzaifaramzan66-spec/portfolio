fbab20cecb10b57a97d8dd9fa834191eaffa547e
huzaifaramzan66-spec <huzaifa.ramzan66@gmail.com>
2026-07-25 08:30:55 +0500

    image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import "./Hero.css";
 import { motion } from "framer-motion";
-import ZombieGame from "../assets/Zombie-game.png";
+import ZombieGame from "../assets/image.png";
 
 function Hero() {
   return (
