f904ceb961f236ffb44331a6a65ee39a4ef4c483
huzaifaramzan66-spec <huzaifa.ramzan66@gmail.com>
2026-07-25 06:25:20 +0500

    whatsapp added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Home from "./pages/Home";
-
+import FloatingWhatsApp from "./components/FloatingWhatsApp";
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <FloatingWhatsApp />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/FloatingWhatsApp.jsx
@@ -0,0 +1,17 @@
+import "./FloatingWhatsApp.css";
+
+function FloatingWhatsApp() {
+  return (
+    <a
+      href="https://wa.me/923104374588"
+      className="floating-whatsapp"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Chat on WhatsApp"
+    >
+      💬
+    </a>
+  );
+}
+
+export default FloatingWhatsApp;
