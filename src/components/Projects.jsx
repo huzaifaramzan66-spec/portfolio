fc53307438a7a8418e33258b69eee9a954c6eacd
huzaifaramzan66-spec <huzaifa.ramzan66@gmail.com>
2026-07-26 09:02:33 +0500

    ....

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -52,7 +52,7 @@ function Projects() {
 </a>
 
 <a
-  href="https://github.com/huzaifaramzan66-spec/Last-Stand-Zombie-Survival"
+  href="https://github.com/huzaifaramzan66-spec"
   target="_blank"
   rel="noopener noreferrer"
   className="github-btn"
